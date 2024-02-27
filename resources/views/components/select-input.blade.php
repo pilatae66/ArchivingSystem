09d9f6273badee843e51e0a1138cc9838d01ab5a
FDC-Magno <60590518+FDC-Magno@users.noreply.github.com>
2024-02-27 20:50:54 +0800

    test

--- resources/views/components/select-input.blade.php
+++ resources/views/components/select-input.blade.php
@@ -1,4 +1,4 @@
-<select {!! $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}>
+<select {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) }}>
     <option value="" disabled hidden>Select unit of measure</option>
     @forelse($options as $option)
         <option value="{{ $option }}" {{ $option === $value ? 'selected' : '' }}>{{ $option }}</option>
