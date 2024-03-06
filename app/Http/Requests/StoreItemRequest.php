db0cbff1be617ffe04fe7dec8bc69dea907a0fc3
FDC-Magno <60590518+FDC-Magno@users.noreply.github.com>
2024-03-06 13:19:23 +0800

    fixing error messages

--- app/Http/Requests/StoreItemRequest.php
+++ app/Http/Requests/StoreItemRequest.php
@@ -25,7 +25,29 @@ class StoreItemRequest extends FormRequest
             'item_code' => ['required','string','max:255'],
             'description' => ['required','string','max:255'],
             'unit_of_measure' => ['required','string','max:255'],
-            'image_url' => ['file','mimes:jpeg,png,jpg,gif,svg'],
+            'img_url' => ['sometimes','file','mimes:jpeg,png,jpg,gif,svg','max:2048'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            //item code validation
+            'item_code.required' => 'Item Code is required.',
+            'item_code.string' => 'Item Code must be a string.',
+            'item_code.max' => 'Item Code must be less than 255 characters.',
+            //description validation
+            'description.required' => 'Description is required.',
+            'description.string' => 'Description must be a string.',
+            'description.max' => 'Description must be less than 255 characters.',
+            //unit of measure validation
+            'unit_of_measure.required' => 'Unit of Measure is required.',
+            'unit_of_measure.string' => 'Unit of Measure must be a string.',
+            'unit_of_measure.max' => 'Unit of Measure must be less than 255 characters.',
+            //image validation
+            'img_url.required' => 'Image is required.',
+            'img_url.mimes' => 'Image must be a file of type: jpeg, png, jpg, gif, svg.',
+            'img_url.max' => 'Image must be less than 2048 KB.',
         ];
     }
 }
--- resources/views/Item/create.blade.php
+++ resources/views/Item/create.blade.php
@@ -10,21 +10,21 @@
                 <!-- Name -->
                 <div>
                     <x-input-label for="item_code" :value="__('Item Code')" />
-                    <x-text-input id="item_code" class="block mt-1 w-full" type="text" name="item_code" :value="old('item_code')" required autofocus autocomplete="item_code" />
+                    <x-text-input id="item_code" class="block mt-1 w-full" type="text" name="item_code" :value="old('item_code')" autofocus autocomplete="item_code" />
                     <x-input-error :messages="$errors->get('item_code')" class="mt-2" />
                 </div>
     
                 <!-- Email Address -->
                 <div class="mt-4">
                     <x-input-label for="description" :value="__('Description')" />
-                    <x-text-area id="description" class="block mt-1 w-full" name="description" :value="old('item_code')" required autocomplete="description" />
+                    <x-text-area id="description" class="block mt-1 w-full" name="description" :value="old('item_code')" autocomplete="description" />
                     <x-input-error :messages="$errors->get('description')" class="mt-2" />
                 </div>
     
                 <!-- Password -->
                 <div class="mt-4">
                     <x-input-label for="unit_of_measure" :value="__('Unit of Measure')" />
-                    <x-select-input id="unit_of_measure" class="block mt-1 w-full" name="unit_of_measure" required autocomplete="unit_of_measure" :options="$unit_of_measures" />
+                    <x-select-input id="unit_of_measure" class="block mt-1 w-full" name="unit_of_measure" autocomplete="unit_of_measure" :options="$unit_of_measures" />
                     <x-input-error :messages="$errors->get('unit_of_measure')" class="mt-2" />
                 </div>
     
