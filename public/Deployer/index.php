1bfbf8e8c21e20e0562c6d46f743acea5e8c32f2
FDC-Magno <60590518+FDC-Magno@users.noreply.github.com>
2024-03-08 13:10:27 +0800

    edit deployer

--- public/Deployer/index.php
+++ public/Deployer/index.php
@@ -1,5 +1,6 @@
 <?php
 define('MAIN_DIR', "/var/www/laravel/public");
+define('ROOT', "/var/www/laravel");
 define('MAIN_BRANCH', "main");
 
 // - check if post is TRUE
@@ -21,28 +22,24 @@ define('MAIN_BRANCH', "main");
 // }
 
 // - commands
-$currentUser = exec('whoami');
-echo "Current user: $currentUser";
 $commandCD = "cd " . MAIN_DIR;
+$commandRootCD = "cd " . ROOT;
 $commandFetch = $commandCD . " && git fetch origin " . MAIN_BRANCH;
 $commandReset = $commandCD . " && git reset --hard FETCH_HEAD";
-$commandNpmInstall = $commandCD . " && npm install";
-$commandNpmBuild = $commandCD . " && npm run build";
+$commandMigration = $commandRootCD . " && php artisan migrate";
 
 // - debug
 var_dump("- RUNNING THE FOLLOWING COMMANDS -");
 var_dump($commandCD);
 var_dump($commandFetch);
 var_dump($commandReset);
-var_dump($commandNpmInstall);
-var_dump($commandNpmBuild);
+var_dump($commandMigration);
 
 // - run commands
 executeCommand($commandCD);
 executeCommand($commandFetch);
 var_dump(executeCommand($commandReset));
-// var_dump(executeCommand($commandNpmInstall));
-// var_dump(executeCommand($commandNpmBuild));
+var_dump(executeCommand($commandMigration));
 
 // - execute command
 function executeCommand($command){
