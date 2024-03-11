062aaf69679d44d5d324978fc1bea5ea4ba811b3
FDC-Magno <60590518+FDC-Magno@users.noreply.github.com>
2024-03-11 13:34:53 +0800

    added design for individual items

--- resources/views/item/show.blade.php
+++ resources/views/item/show.blade.php
@@ -19,28 +19,46 @@
             <div class="border-b-2 border-gray-500 my-10"></div>
             <div class="grid grid-cols-4">
                 <div class="flex flex-col justify-center items-center py-10 border-r-2 border-gray-500">
-                    <p>Item Code</p>
+                    <p class="flex flex-col justify-center items-center">
+                        <x-fas-barcode class="w-16 h-16" />
+                        Item Code
+                    </p>
                     <h1 class="text-3xl">{{ $item->item_code }}</h1>
                 </div>
                 <div class="flex flex-col justify-center items-center py-10 border-r-2 border-gray-500">
-                    <p>Description</p>
+                    <p class="flex flex-col justify-center items-center">
+                        <x-fluentui-text-description-20 class="w-16 h-16" />
+                        Description
+                    </p>
                     <h1 class="text-3xl">{{ $item->description }}</h1>
                 </div>
                 <div class="flex flex-col justify-center items-center py-10 border-r-2 border-gray-500">
-                    <p>Unit of Measurement</p>
+                    <p class="flex flex-col justify-center items-center">
+                        <x-tabler-ruler-measure class="w-16 h-16" />
+                        Unit of Measurement
+                    </p>
                     <h1 class="text-3xl">{{ $item->unit_of_measure }}</h1>
                 </div>
                 <div class="flex flex-col justify-center items-center py-10">
-                    <p>Department</p>
+                    <p class="flex flex-col justify-center items-center">
+                        <x-vaadin-group class="w-16 h-16" />
+                        Department
+                    </p>
                     <h1 class="text-3xl">{{ $item->department }}</h1>
                 </div>
                 <div></div>
                 <div class="flex flex-col justify-center items-center border-r-2 border-gray-500 py-10">
-                    <p>End User</p>
+                    <p class="flex flex-col justify-center items-center">
+                        <x-ri-group-line class="w-16 h-16" />
+                        End User
+                    </p>
                     <h1 class="text-3xl">{{ $item->end_user }}</h1>
                 </div>
                 <div class="flex flex-col justify-center items-center py-10">
-                    <p>Requestor</p>
+                    <p class="flex flex-col justify-center items-center">
+                        <x-tabler-user-dollar class="w-16 h-16" />
+                        Requestor
+                    </p>
                     <h1 class="text-3xl">{{ $item->requestor }}</h1>
                 </div>
             </div>
